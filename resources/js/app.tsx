45459cdf869c8eb90e32e60a5056b609d696cda4
illuminateor <o.mudrak93@gmail.com>
2026-05-30 17:42:41 +0200

    Upgrade npm packages including to inertia 3

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -2,23 +2,27 @@ import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import type { ComponentType } from 'react';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => title ? `${title} - ${appName}` : appName,
-    resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
-    setup({ el, App, props }) {
-        const root = createRoot(el);
+    title: (title) => (title ? `${title} - ${appName}` : appName),
 
-        root.render(<App {...props} />);
+    resolve: async (name) => {
+        const module = await resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob<{ default: ComponentType }>('./Pages/**/*.tsx'));
+        return module.default;
     },
+    setup({ App, props }) {
+        const element = document.getElementById('app') ?? document.body.appendChild(document.createElement('div'));
+        createRoot(element).render(<App {...props} />);
+    },
+
     progress: {
         color: '#4B5563',
     },
 });
 
-// This will set light / dark mode on load...
 initializeTheme();
